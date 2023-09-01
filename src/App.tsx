22bf3609aa5f4d1420d0d3c7d11c98408dd94df9
neerajsu <neeraj.subramaniyan@gmail.com>
2023-09-01 17:31:25 +0530

    Build a top navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         }
       }>
 
-        <GridItem area='nav' bg='coral'>Nav</GridItem>
+        <GridItem area='nav'>
+          <NavBar></NavBar>
+        </GridItem>
         <Show above='lg'>
           <GridItem area='aside' bg='gold'>Aside</GridItem>
         </Show>
