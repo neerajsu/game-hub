2170c4960f821c415785553bf5f217683a69f632
neerajsu <neeraj.subramaniyan@gmail.com>
2023-09-02 14:16:57 +0530

    Refactor - remove duplicated styles
    - GameCard component.
    - GameCardSkeleton component.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius={10} overflow='hidden'>
+        <Card>
             <Image src={getCroppedImageUrl(game.background_image)}></Image>
             <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
+                <Heading fontSize='xl'>{game.name}</Heading>
                 <HStack justifyContent='space-between'>
                     <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
                     <CriticScore score={game.metacritic} />
