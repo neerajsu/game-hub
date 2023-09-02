88cf08c2109d3b1a95690718de2d258c8fdb8809
neerajsu <neeraj.subramaniyan@gmail.com>
2023-09-02 13:20:59 +0530

    Remove React strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import './index.css'
 import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </ChakraProvider>
+
 )
